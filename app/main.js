adfb135733b636f08096e0872b9f2032f4f07a27
Apfelclonk <apfelclonk@googlemail.com>
2016-09-29 15:36:08 +0200

    menu bar now hidden in !DevMode

--- app/main.js
+++ app/main.js
@@ -41,7 +41,9 @@ function createWindow () {
 	
 	// Create the browser window.
 	win = new BrowserWindow(wininfo.opts)
-	win.setMenu(null)
+	
+	if(!wininfo.enableDevMode)
+		win.setMenu(null)
 	
 	if(wininfo.opts.maximized)
 		win.maximize()
