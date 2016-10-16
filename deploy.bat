c38e2e5df8eb012b9258648b7a4572959750780e
Apfelclonk <apfelclonk@googlemail.com>
2016-10-16 14:15:12 +0200

    reworked deploy instructions

--- deploy.bat
+++
@@ -1,8 +0,0 @@
-#pseudo bat
-#doesnt work
-#need to copy past it
-run command as admin to deploy OSx 
-
-npm install C:\nanoMill\nanoMill\app --save
-electron-packager C:\nanoMill\nanoMill\app nanomill --all --asar --platform=win32 --version=1.4.1 --overwrite --out=C:\nanoMill\nanoMill\pre_dist --icon=C:\nanoMill\nanoMill\app\the-mill.ico
-pause
