d9f3037213822ad6e85b7b8b587f5e2d95e76177
Apfelclonk <apfelclonk@googlemail.com>
2016-10-16 16:35:28 +0200

    changed text shadow of introscreen

--- app/js/modules/introscreen.js
+++ app/js/modules/introscreen.js
@@ -6,8 +6,8 @@ class IntroScreen extends Layout_Module{
 		$(this.root).find(".mod-body").html(
 "<div class='flex-col mod-fill-body' style='animation: fade-in 1s ease-out 0s 1 normal both'>\
 	<div class='flex-fill'></div>\
-		<span style='text-align: center'>Welcome .</span><br>\
-		<span style='text-align: center; color: #1d1d1d; font-size: 32px; text-shadow: 0 -1px 0 #101010, -2px 0 #1f1f1f' class='icon-the-mill'></span>\
+		<span style='text-align: center; text-shadow: 0 1px 0 black'>Welcome .</span><br>\
+		<span style='text-align: center; color: #1d1d1d; font-size: 32px; text-shadow: 0 -1px 0 #101010, 0 -2px 0 #1f1f1f, 0 -3px 0 #2b2b2b, 0 1px 0 #404040' class='icon-the-mill'></span>\
 	<div class='flex-fill'></div>\
 </div>"
 		)
